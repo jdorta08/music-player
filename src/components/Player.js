11fd87eed8fc8282cdbe63e3c82370812f24529b
jdorta08 <dortajavier.jd2@gmail.com>
2023-01-25 16:33:19 -0500

    Added Song and Player Components

---
+++ src/components/Player.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Player = () => {
+  return (
+    <div className="player-container">
+      <p>Start Time</p>
+      <input type="range"></input>
+      <p>End Time</p>
+    </div>
+  );
+};
+
+export default Player;
