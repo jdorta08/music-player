20c037267ecd51f86085194de715d812eebccc94
jdorta08 <dortajavier.jd2@gmail.com>
2023-01-25 17:14:28 -0500

    added font awesome

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Player = () => {
   return (
     <div className="player-container">
-      <p>Start Time</p>
-      <input type="range"></input>
-      <p>End Time</p>
+      <div className="time-control">
+        <p>Start Time</p>
+        <input type="range"></input>
+        <p>End Time</p>
+      </div>
+      <div className="player-control">
+        <FontAwesomeIcon className="play" icon={faPlay} />
+        <FontAwesomeIcon className="play" icon={faAngleLeft} />
+        <FontAwesomeIcon className="play" icon={faAngleRight} />
+      </div>
     </div>
   );
 };
