1aa23ee2e431b4e6c04e2e2865c294f10e751448
jdorta08 <dortajavier.jd2@gmail.com>
2023-01-27 16:30:51 -0500

    added styles and added the data.js with song data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 //Import Styles
 import "./styles/style.scss";
 //Import Components
 import Song from "./components/Song";
 import Player from "./components/Player";
+//Import Music Data
+import data from "./data";
 
 function App() {
+  //state
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
